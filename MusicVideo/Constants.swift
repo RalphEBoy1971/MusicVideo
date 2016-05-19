d4976320976da35295c8c8a0d37c8bd06b4673da
Ralph Halbrook <rhalbrook@consoloservices.com>
2016-05-19 15:38:18 -0400

    Part 4 Completed

---
+++ MusicVideo/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  MusicVideo
+//
+//  Created by Ralph Halbrook on 5/19/16.
+//  Copyright © 2016 Ralph Halbrook. All rights reserved.
+//
+
+import Foundation
+
+typealias JSONDictionary = [String: AnyObject]
+
+typealias JSONArray = Array<AnyObject>
+
+
