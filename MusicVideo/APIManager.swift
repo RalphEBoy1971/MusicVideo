ff65adb11fad1218fe0bbd8bad7b6550064a957a
Ralph Halbrook <ralph@halbrooksonline.com>
2016-08-12 10:47:11 -0400

    Completed Part 6

--- MusicVideo/APIManager.swift
+++ MusicVideo/APIManager.swift
@@ -11,7 +11,7 @@ import Foundation
 class APIManager {
 
     
-    func loadData(urlString:String, completion: (result:String) -> Void) {
+    func loadData(urlString:String, completion: [Videos] -> Void) {
         
         let config = NSURLSessionConfiguration.ephemeralSessionConfiguration()
         let session = NSURLSession(configuration: config)
@@ -23,9 +23,9 @@ class APIManager {
             (data, response, error) -> Void in
             
             if error != nil {
-                dispatch_async(dispatch_get_main_queue()) {
-                    completion(result: (error!.localizedDescription))
-                }
+
+                print(error!.localizedDescription)
+
             } else {
                 
                 //Added for JSONSerialization
@@ -36,23 +36,35 @@ class APIManager {
                     NSJSONSerialization requires the Do / Try / Catch
                     Converts the NSDATA into a JSON Object and cast it to a Dictionary */
                     
-                    if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as? JSONDictionary {
+                    if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as? JSONDictionary, feed = json["feed"] as? JSONDictionary,
+                        entries = feed["entry"] as? JSONArray {
                         
-                        print(json)
+                        var videos = [Videos]()
+                        for entry in entries {
+                            let entry = Videos(data: entry as! JSONDictionary)
+                            videos.append(entry)
+                        }
                         
-                        let priority = DISPATCH_QUEUE_PRIORITY_HIGH
+                        let i = videos.count
+                        print("ITunesApiManager - total count --> \(i)")
+                        print(" ")
+                        
+                        let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
                         dispatch_async(dispatch_get_global_queue(priority, 0)) {
                             dispatch_async(dispatch_get_main_queue()) {
-                                completion(result: "JSONSerialization Successful")
+                                completion(videos)
                             }
                         }
+                        
+                        
                     }
+                    
+                    
                 } catch {
-                    dispatch_async(dispatch_get_main_queue()) {
-                        completion(result: "error in NSJSONSerialization")
-                    }
+
+                    print("error in NSJSONSerialization")
+
                 }
-                //End of JSONSerialization
             }
             }
         task.resume()
