02b78374bb8d3e449dfbca5b713daec7b5233f48
Ralph Halbrook <ralph@halbrooksonline.com>
2016-08-31 16:43:32 -0400

    Completed Part 15

--- MusicVideo/MusicVideoTableViewCell.swift
+++ MusicVideo/MusicVideoTableViewCell.swift
@@ -24,8 +24,43 @@ class MusicVideoTableViewCell: UITableViewCell {
         
         musicTitle.text = video?.vName
         rank.text = ("\(video!.vRank)")
-        musicImage.image = UIImage(named: "itunes")
+        
+        //musicImage.image = UIImage(named: "itunes")
+        
+        if video!.vImageData != nil {
+            print("Get data from array ...")
+            musicImage.image = UIImage(data: video!.vImageData!)
+        }
+        else
+        {
+            getVideoImage(video!, imageView: musicImage)
+        }
+        
         
     }
+    
+    func getVideoImage(video: Videos, imageView: UIImageView){
+        
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+            
+            let data = NSData(contentsOfURL: NSURL(string: video.vImageUrl)!)
+            
+            var image : UIImage?
+            if data != nil {
+                video.vImageData = data
+                image = UIImage(data: data!)
+            }
+            
+            // move back to Main Queue
+            dispatch_async(dispatch_get_main_queue()) {
+                imageView.image = image
+            }
+        }
+        
+
+    }
+    
+    
     
 }
