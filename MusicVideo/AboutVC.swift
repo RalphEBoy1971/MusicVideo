8534c8590f6c69c4e56a061bf6d307dae2a50ad0
Ralph Halbrook <ralph@halbrooksonline.com>
2016-09-01 16:44:00 -0400

    Exercise: Add About Page

---
+++ MusicVideo/AboutVC.swift
@@ -0,0 +1,21 @@
+//
+//  AboutVC.swift
+//  MusicVideo
+//
+//  Created by Ralph Halbrook on 9/1/16.
+//  Copyright © 2016 Ralph Halbrook. All rights reserved.
+//
+
+import UIKit
+
+class AboutVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "About"
+
+        // Do any additional setup after loading the view.
+    }
+
+}
