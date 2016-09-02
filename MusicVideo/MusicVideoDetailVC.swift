e2f4c174a904239b6a3f2c02790c23c41782ac21
Ralph Halbrook <ralph@halbrooksonline.com>
2016-09-02 14:11:36 -0400

    Completed Part 25

--- MusicVideo/MusicVideoDetailVC.swift
+++ MusicVideo/MusicVideoDetailVC.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class MusicVideoDetailVC: UIViewController {
     
@@ -50,4 +52,24 @@ class MusicVideoDetailVC: UIViewController {
     deinit {
         NSNotificationCenter.defaultCenter().removeObserver(self, name: UIContentSizeCategoryDidChangeNotification, object: nil)
     }
+    
+    @IBAction func playVideo(sender: UIBarButtonItem) {
+        
+        let url = NSURL(string: videos.vVideoUrl)!
+        
+        let player = AVPlayer(URL: url)
+        
+        let playerViewController = AVPlayerViewController()
+        
+        playerViewController.player = player
+        
+        self.presentViewController(playerViewController, animated: true) {
+            playerViewController.player?.play()
+            
+        }
+        
+        
+        
+    }
+    
 }
