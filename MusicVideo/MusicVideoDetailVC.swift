7748981533c30ebc83dba8c3be3944f80aaa3f42
Ralph Halbrook <ralph@halbrooksonline.com>
2016-09-06 13:51:58 -0400

    Fixed Part 29

--- MusicVideo/MusicVideoDetailVC.swift
+++ MusicVideo/MusicVideoDetailVC.swift
@@ -11,6 +11,7 @@ import AVKit
 import AVFoundation
 import LocalAuthentication
 
+
 class MusicVideoDetailVC: UIViewController {
     
     var videos:Videos!
@@ -18,19 +19,20 @@ class MusicVideoDetailVC: UIViewController {
     var securitySwitch:Bool = false
     
     @IBOutlet weak var vName: UILabel!
+    
     @IBOutlet weak var videoImage: UIImageView!
+    
     @IBOutlet weak var vGenre: UILabel!
+    
     @IBOutlet weak var vPrice: UILabel!
+    
     @IBOutlet weak var vRights: UILabel!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "preferredFontChange", name: UIContentSizeCategoryDidChangeNotification, object: nil)
-
         
         title = videos.vArtist
-        
         vName.text = videos.vName
         vPrice.text = videos.vPrice
         vRights.text = videos.vRights
@@ -38,27 +40,16 @@ class MusicVideoDetailVC: UIViewController {
         
         if videos.vImageData != nil {
             videoImage.image = UIImage(data: videos.vImageData!)
-        } else {
+        }
+        else {
             videoImage.image = UIImage(named: "imageNotAvailable")
         }
-    }
-    
-    func preferredFontChange() {
         
-        vName.font = UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline)
-        vGenre.font = UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline)
-        vPrice.font = UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline)
-        vRights.font = UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline)
-    }
-    
-    deinit {
-        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIContentSizeCategoryDidChangeNotification, object: nil)
     }
     
     
     @IBAction func socialMedia(sender: UIBarButtonItem) {
-
-        securitySwitch = NSUserDefaults.standardUserDefaults().boolForKey("SecSettings")
+        securitySwitch = NSUserDefaults.standardUserDefaults().boolForKey("SecSetting")
         
         switch securitySwitch {
         case true:
@@ -69,16 +60,20 @@ class MusicVideoDetailVC: UIViewController {
         
     }
     
+    
+    
     func touchIdChk() {
         
-        // Creat an alert
+        // Create an alert
         let alert = UIAlertController(title: "", message: "", preferredStyle: UIAlertControllerStyle.Alert)
         alert.addAction(UIAlertAction(title: "continue", style: UIAlertActionStyle.Cancel, handler: nil))
+        
         
         // Create the Local Authentication Context
         let context = LAContext()
         var touchIDError : NSError?
         let reasonString = "Touch-Id authentication is needed to share info on Social Media"
+        
         
         // Check if we can access local device authentication
         if context.canEvaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, error:&touchIDError) {
@@ -94,22 +89,31 @@ class MusicVideoDetailVC: UIViewController {
                     alert.title = "Unsuccessful!"
                     
                     switch LAError(rawValue: policyError!.code)! {
+                        
                     case .AppCancel:
                         alert.message = "Authentication was cancelled by application"
+                        
                     case .AuthenticationFailed:
                         alert.message = "The user failed to provide valid credentials"
+                        
                     case .PasscodeNotSet:
                         alert.message = "Passcode is not set on the device"
+                        
                     case .SystemCancel:
                         alert.message = "Authentication was cancelled by the system"
+                        
                     case .TouchIDLockout:
-                        alert.message = "Too many failed attempts"
+                        alert.message = "Too many failed attempts."
+                        
                     case .UserCancel:
                         alert.message = "You cancelled the request"
+                        
                     case .UserFallback:
                         alert.message = "Password not accepted, must use Touch-ID"
+                        
                     default:
                         alert.message = "Unable to Authenticate!"
+                        
                     }
                     
                     // Show the alert
@@ -126,14 +130,19 @@ class MusicVideoDetailVC: UIViewController {
             
             // Set the error alert message with more information
             switch LAError(rawValue: touchIDError!.code)! {
+                
             case .TouchIDNotEnrolled:
                 alert.message = "Touch ID is not enrolled"
+                
             case .TouchIDNotAvailable:
-                alert.message = "Touch ID is not available on the device"
+                alert.message = "TouchID is not available on the device"
+                
             case .PasscodeNotSet:
                 alert.message = "Passcode has not been set"
+                
             case .InvalidContext:
                 alert.message = "The context is invalid"
+                
             default:
                 alert.message = "Local Authentication not available"
             }
@@ -143,20 +152,24 @@ class MusicVideoDetailVC: UIViewController {
                 self.presentViewController(alert, animated: true, completion: nil)
             }
         }
+        
+        
     }
+    
     
     func shareMedia() {
         
-        let activity1 = "Have you had the opportunity to see thie Music Video"
+        let activity1 = "Have you had the opportunity to see this Music Video?"
         let activity2 = ("\(videos.vName) by \(videos.vArtist)")
-        let activity3 = "Watch it and tell me what you think"
+        let activity3 = "Watch it and tell me what you think?"
         let activity4 = videos.vLinkToiTunes
-        let activity5 = "(Shared with the Music Video App - Step It Up!)"
+        let activity5 = "(Shared with the Music Video App - Step It UP!)"
         
-        let activityViewController : UIActivityViewController = UIActivityViewController(activityItems: [activity1, activity2, activity3, activity4, activity5], applicationActivities: nil)
+        let activityViewController : UIActivityViewController = UIActivityViewController(activityItems: [activity1, activity2, activity3, activity4,activity5], applicationActivities: nil)
         
-        // FOR EXCLUDING ITEMS
         //activityViewController.excludedActivityTypes =  [UIActivityTypeMail]
+        
+        
         
         //        activityViewController.excludedActivityTypes =  [
         //            UIActivityTypePostToTwitter,
@@ -173,18 +186,22 @@ class MusicVideoDetailVC: UIViewController {
         //            UIActivityTypePostToVimeo,
         //            UIActivityTypePostToTencentWeibo
         //        ]
-
+        
         activityViewController.completionWithItemsHandler = {
             (activity, success, items, error) in
             
             if activity == UIActivityTypeMail {
-                print("email selected")
+                print ("email selected")
             }
+            
         }
         
         self.presentViewController(activityViewController, animated: true, completion: nil)
         
+        
     }
+    
+    
     
     @IBAction func playVideo(sender: UIBarButtonItem) {
         
@@ -198,11 +215,10 @@ class MusicVideoDetailVC: UIViewController {
         
         self.presentViewController(playerViewController, animated: true) {
             playerViewController.player?.play()
-            
-        }
-        
-        
+        } 
         
     }
+    
+    
     
 }
